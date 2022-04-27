080d675f5c4d871124d1fbe12b228d913eefdb71
Mohammad Zouman <mohammadbinzouman@gmail.com>
2022-04-27 23:06:09 +0300

    update code files

--- Code/Dragon.php
+++ Code/Dragon.php
@@ -3,8 +3,10 @@
     $username='AQ';
     $password='AQ123456';
     $dbname = "dragon";
+
     $conn=mysqli_connect($servername,$username,$password,"$dbname");
-      if(!$conn){
-          die('Could not Connect MySql Server:' .mysql_error());
-        }
+    
+    if(!$conn){
+      die('Could not Connect MySql Server:' .mysql_error());
+    }
 ?>
